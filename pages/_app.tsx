88e0c7b5fdf4cf299f13d774b6ee607dbfeeb506
Summer Chen <mp4bj6ruby@gmail.com>
2021-02-05 11:39:35 +0800

    added Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,8 @@ import AlertProvider from '../context/AlertProvider'
 import GlobalProvider from '../context/GlobalProvider'
 import LoaderProvider from '../context/LoaderProvider'
 import DateProvider from '../context/DateProvider'
-
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../lib/theme'
 import 'swiper/swiper.scss'
 import '../style/global.scss'
 
@@ -11,11 +12,13 @@ const MyApp = ({ Component, props }) => {
   return (
     <GlobalProvider>
       <DateProvider>
-        <LoaderProvider>
-          <AlertProvider>
-            <Component {...props} />
-          </AlertProvider>
-        </LoaderProvider>
+        <ChakraProvider theme={theme}>
+          <LoaderProvider>
+            <AlertProvider>
+              <Component {...props} />
+            </AlertProvider>
+          </LoaderProvider>
+        </ChakraProvider>
       </DateProvider>
     </GlobalProvider>
   )
