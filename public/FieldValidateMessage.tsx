b9bb839f06c819511b9d9a23911075c2107d12d0
Summer Chen <mp4bj6ruby@gmail.com>
2021-02-09 10:19:39 +0800

    resource updated

--- public/FieldValidateMessage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { FieldError } from 'react-hook-form/dist/types'
-
-const FieldValidateMessage: React.FC<{ error?: FieldError }> = ({ error }) => {
-  if (!error) return <></>
-  return <span className="text-danger">{error.message}</span>
-}
-
-export default FieldValidateMessage
