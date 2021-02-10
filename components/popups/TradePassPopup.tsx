971efec1093a87a0adbd10d9edbb1d3e4997cbc7
Summer Chen <mp4bj6ruby@gmail.com>
2021-02-10 10:58:22 +0800

    update trade password

--- components/popups/TradePassPopup.tsx
+++ components/popups/TradePassPopup.tsx
@@ -1,13 +1,40 @@
-import $ from 'jquery'
 import React, { useEffect } from 'react'
-import BottomPopup from './BottomPopup'
+import BottomPopup from '@/components/popups/BottomPopup'
+import $ from 'jquery'
+import { useForm } from 'react-hook-form'
+import useRequest from '@/utils/useRequest'
+import { useToast } from '@chakra-ui/toast'
+import useService from '@/utils/useService'
+import FieldValidateMessage from '../FieldValidateMessage'
+import pattern from '@/lib/pattern'
 
 const jqEffectFunc = function () {
   $('.mask').fadeIn()
   $('#withdrawal-pw-edit').addClass('slide-up')
 }
 
-function TradePassPopup() {
+interface FormProps {
+  old_pass: string
+  pass: string
+  pass_confirm: string
+}
+
+function ProfileTradePassPopup() {
+  const { register, handleSubmit, errors, reset, watch } = useForm<FormProps>()
+  const API = useRequest()
+  const toast = useToast()
+
+  const onSubmit = handleSubmit(async (d) => {
+    try {
+      await API.updateTradePw({
+        old_pass: d.old_pass,
+        pass: d.pass,
+      })
+      toast({ status: 'success', title: '更新成功' })
+      $('.mask').fadeOut()
+      $('.slide-up-section').removeClass('slide-up')
+    } catch (err) {}
+  })
   useEffect(() => {
     $('.withdrawal-pw').on('click', jqEffectFunc)
     return () => {
@@ -15,43 +42,52 @@ function TradePassPopup() {
     }
   }, [])
   return (
-    <BottomPopup title="提领密码" id="withdrawal-pw-edit">
-      <form>
+    <BottomPopup title="提領密码修改" id="withdrawal-pw-edit" onClose={reset}>
+      <form onSubmit={onSubmit} noValidate>
         <label className="form-label">当前密码</label>
         <div className="form-group">
           <input
             type="password"
             className="form-input account-input"
-            required
             placeholder="请输入旧密码"
+            name="old_pass"
+            ref={register({ required: '不可為空' })}
           />
-          <a className="iconfont iconclear btn_cancel"></a>
+          <FieldValidateMessage error={errors.old_pass} />
         </div>
         <label className="form-label">新密码</label>
         <div className="form-group">
           <input
             type="password"
-            className="form-input account-input"
-            required
-            placeholder="请输入确认密码"
+            className="form-input"
+            placeholder="新密码"
+            name="pass"
+            ref={register({
+              required: '不可為空',
+              pattern: {
+                value: pattern.pass,
+                message: '格式有誤',
+              },
+            })}
           />
-          <a className="iconfont iconclear btn_cancel"></a>
+          <FieldValidateMessage error={errors.pass} />
         </div>
         <label className="form-label">确认新密码</label>
         <div className="form-group">
           <input
             type="password"
-            className="form-input account-input"
-            required
-            placeholder="再次确认密码"
+            className="form-input"
+            placeholder="确认新密码"
+            name="pass_confirm"
+            ref={register({
+              required: '不可為空',
+              validate: (value) =>
+                value !== watch('pass') ? '密码不同' : true,
+            })}
           />
-          <a className="iconfont iconclear btn_cancel"></a>
+          <FieldValidateMessage error={errors.pass_confirm} />
         </div>
-
-        <button
-          type="submit"
-          className="btnbase primary_btn mt-4 mb-2 remove-slide"
-        >
+        <button type="submit" className="btnbase primary_btn mt-4 mb-2">
           送出
         </button>
       </form>
@@ -59,4 +95,4 @@ function TradePassPopup() {
   )
 }
 
-export default TradePassPopup
+export default ProfileTradePassPopup
