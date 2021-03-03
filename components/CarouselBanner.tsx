f07f236fc3f92dfdf260d70299305d93acd88194
Summer Chen <mp4bj6ruby@gmail.com>
2021-03-03 21:26:46 +0800

    CarouselBanner: link, event to events

--- components/CarouselBanner.tsx
+++ components/CarouselBanner.tsx
@@ -1,8 +1,12 @@
 import { Banner } from '@/lib/types'
+import { Image } from '@chakra-ui/image'
 import { useRouter } from 'next/dist/client/router'
+import Link from 'next/link'
 import React from 'react'
+import SwiperCore, { Pagination, Autoplay } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
+SwiperCore.use([Pagination, Autoplay])
 const CarouselBanner: React.FC<{ banners: Banner[] }> = ({ banners }) => {
   const router = useRouter()
   return (
@@ -13,15 +17,19 @@ const CarouselBanner: React.FC<{ banners: Banner[] }> = ({ banners }) => {
         autoplay={{ delay: 3000 }}
         speed={500}
         className="swiper-container"
-        pagination={{ el: '.swiper-pagination', clickable: true }}
+        pagination={{ clickable: true }}
       >
         {banners.map((t, i) => (
-          <SwiperSlide
-            className="swiper-slide"
-            key={i}
-            onClick={() => router.push(t.url)}
-          >
-            <img src={t.img_mobile} className="d-block w-100" />
+          <SwiperSlide className="swiper-slide" key={i}>
+            {t.url ? (
+              <Link href={t.url}>
+                <a target={t.is_blank ? '_blank' : '_self'}>
+                  <Image src={t.img_mobile} />
+                </a>
+              </Link>
+            ) : (
+              <Image src={t.img_mobile} />
+            )}
           </SwiperSlide>
         ))}
       </Swiper>
