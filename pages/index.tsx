2dee936fdba9333453504dd4f121082cebb464a1
Summer Chen <mp4bj6ruby@gmail.com>
2021-03-19 00:35:13 +0800

    index page ui test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,63 @@
+import FieldValidateMessage from '@/components/FieldValidateMessage'
 import Layout from '@/components/Layout'
-import useRequest from '@/utils/useRequest'
-import { Center, Spinner } from '@chakra-ui/react'
-import { useRouter } from 'next/dist/client/router'
-import React, { useEffect } from 'react'
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+} from '@chakra-ui/react'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+interface LoginFormProps {
+  acc: string
+  pass: string
+}
 
 const IndexPage = () => {
+  const { register, errors } = useForm<LoginFormProps>()
+
   return (
     <Layout>
-      <Center h="100vh" bg="gray.100">
-        <h1>Hello World</h1>
+      <Center h="100vh" bg="pink.200">
+        <Box
+          as="form"
+          pb="60px"
+          bg="white"
+          py="40px"
+          px="50px"
+          shadow="md"
+          w="full"
+        >
+          <Heading mb="30px" color="gray.600" align="center">
+            LOGIN PAGE
+          </Heading>
+          <Stack spacing="20px">
+            <FormControl isRequired>
+              <FormLabel>帳號</FormLabel>
+              <Input placeholder="帳號" ref={register({ required: true })} />
+              <FieldValidateMessage error={errors.acc} />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>密碼</FormLabel>
+              <Input placeholder="密碼" ref={register({ required: true })} />
+              <FieldValidateMessage error={errors.pass} />
+            </FormControl>
+            <Button
+              mt={4}
+              colorScheme="gray"
+              w="full"
+              // isLoading={props.isSubmitting}
+              type="submit"
+            >
+              送出
+            </Button>
+          </Stack>
+        </Box>
       </Center>
     </Layout>
   )
