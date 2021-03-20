3bf5361dde3f0f1499591b034c2b13d7d8a815fa
Summer Chen <mp4bj6ruby@gmail.com>
2021-03-20 11:12:10 +0800

    bankcard detail

--- lib/options.ts
+++ lib/options.ts
@@ -1,7 +1,12 @@
-import { DateRangeType } from './enums'
+import { BankCardStatus, DateRangeType } from './enums'
 
 export const dateRangeOpts = [
   { label: '今日', value: DateRangeType.Today },
   { label: '本週', value: DateRangeType.ThisWeek },
   { label: '本月', value: DateRangeType.ThisMonth },
+]
+
+export const bankcardStatusOpts = [
+  { label: '启用', value: BankCardStatus.On },
+  { label: '未启用', value: BankCardStatus.Off },
 ]
--- pages/bankcard/index.tsx
+++ pages/bankcard/index.tsx
@@ -2,17 +2,39 @@ import FooterNav from '@/components/FooterNav'
 import HeaderTitleBar from '@/components/HeaderTitleBar'
 import Layout from '@/components/Layout'
 import { BankCardStatus } from '@/lib/enums'
+import { bankcardStatusOpts } from '@/lib/options'
+import { BankCard } from '@/lib/types'
 import useService from '@/utils/useService'
+import useTransfer from '@/utils/useTransfer'
+import { Button } from '@chakra-ui/button'
+import { useDisclosure } from '@chakra-ui/hooks'
 import { Box, HStack, Stack, Text } from '@chakra-ui/layout'
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/modal'
 import { Tag } from '@chakra-ui/tag'
-import React from 'react'
+import React, { useState } from 'react'
 
 function bankcardList() {
+  const { isOpen, onClose, onOpen } = useDisclosure()
+  const [currentCard, setCurrentCard] = useState<BankCard>(null)
   const { useBankList } = useService()
+  const { toOptionName } = useTransfer()
   const { data: res, error } = useBankList()
+
+  const handleClickCard = (data: BankCard) => {
+    setCurrentCard(data)
+    onOpen()
+  }
   return (
     <Layout>
-      <HeaderTitleBar back title="銀行卡管理" />
+      <HeaderTitleBar back title="银行卡管理" />
       <Box flex="1" overflowY="auto" p="20px">
         <Stack spacing="15px">
           {res?.list.map((t) => (
@@ -24,6 +46,7 @@ function bankcardList() {
               shadow="md"
               // borderWidth="3px"
               // borderColor="purple.100"
+              onClick={() => handleClickCard(t)}
             >
               <HStack justify="space-between">
                 <Text color="gray.900" fontSize="md" fontWeight="600">
@@ -34,7 +57,7 @@ function bankcardList() {
                   borderRadius="4px"
                   colorScheme={t.status === BankCardStatus.On ? 'pink' : 'gray'}
                 >
-                  {t.status === BankCardStatus.On ? '啟用中' : '未啟用'}
+                  {toOptionName(bankcardStatusOpts, t.status)}
                 </Tag>
               </HStack>
               <Text fontSize="30px" fontWeight="600" color="purple.600">
@@ -48,6 +71,82 @@ function bankcardList() {
         </Stack>
       </Box>
       <FooterNav />
+
+      <Modal isOpen={isOpen} onClose={onClose} autoFocus={false}>
+        <ModalOverlay />
+        <ModalContent mx="20px">
+          <ModalHeader>我的银行信息</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {currentCard && (
+              <>
+                <Box borderBottom="1px solid #ccc" py="10px">
+                  <Text fontSize="sm" mb="5px">
+                    状态
+                  </Text>
+                  <Text
+                    fontSize="lg"
+                    fontWeight="600"
+                    color={
+                      currentCard.status === BankCardStatus.On
+                        ? 'pink.500'
+                        : 'gray.500'
+                    }
+                  >
+                    {toOptionName(bankcardStatusOpts, currentCard.status)}
+                  </Text>
+                </Box>
+                <Box borderBottom="1px solid #ccc" py="10px">
+                  <Text fontSize="sm" mb="5px">
+                    银行卡姓名
+                  </Text>
+                  <Text fontSize="lg" fontWeight="600">
+                    {currentCard.name}
+                  </Text>
+                </Box>
+                <Box borderBottom="1px solid #ccc" py="10px">
+                  <Text fontSize="sm" mb="5px">
+                    银行名称
+                  </Text>
+                  <Text fontSize="lg" fontWeight="600">
+                    {currentCard.bank}
+                  </Text>
+                </Box>
+                <Box borderBottom="1px solid #ccc" py="10px">
+                  <Text fontSize="sm" mb="5px">
+                    支行名称
+                  </Text>
+                  <Text fontSize="lg" fontWeight="600">
+                    {currentCard.bank_name}
+                  </Text>
+                </Box>
+                <Box borderBottom="1px solid #ccc" py="10px">
+                  <Text fontSize="sm" mb="5px">
+                    银行帐号
+                  </Text>
+                  <Text fontSize="lg" fontWeight="600">
+                    {currentCard.account}
+                  </Text>
+                </Box>
+                <Box borderBottom="1px solid #ccc" py="10px">
+                  <Text fontSize="sm" mb="5px">
+                    开户省份 / 城市
+                  </Text>
+                  <Text fontSize="lg" fontWeight="600">
+                    {currentCard.province} / {currentCard.city}
+                  </Text>
+                </Box>
+              </>
+            )}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="purple" onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Layout>
   )
 }
