7a2e506b7e9055c4c0bc794e73077ac7bbd0511d
Summer Chen <mp4bj6ruby@gmail.com>
2021-06-02 09:26:56 +0800

    bankcard optimized

--- pages/bankcard/index.tsx
+++ pages/bankcard/index.tsx
@@ -11,6 +11,7 @@ import useTransfer from '@/utils/useTransfer'
 import { Button } from '@chakra-ui/button'
 import { useDisclosure } from '@chakra-ui/hooks'
 import { Box, HStack, Stack, Text } from '@chakra-ui/layout'
+import { Spinner } from '@chakra-ui/spinner'
 import { Tag } from '@chakra-ui/tag'
 import { useRouter } from 'next/dist/client/router'
 import React, { useState } from 'react'
@@ -19,7 +20,7 @@ function bankcardList() {
   const { isOpen, onClose, onOpen } = useDisclosure()
   const [currentCard, setCurrentCard] = useState<BankCard>(null)
   const { toOptionName } = useTransfer()
-  const { bankcardList } = useBankCardList()
+  const { bankcardList, isLoading } = useBankCardList()
   const router = useRouter()
 
   const handleClickCard = (data: BankCard) => {
@@ -30,46 +31,53 @@ function bankcardList() {
     <Layout>
       <HeaderTitleBar back title="银行卡管理" />
       <Box flex="1" overflowY="auto" p="20px" pb="50px">
-        <Stack spacing="15px">
-          <Button
-            colorScheme="pink"
-            w="full"
-            onClick={() => router.push('/bankcard/add')}
-          >
-            新增银行卡
-          </Button>
-          {bankcardList?.map((t) => (
-            <Stack
-              key={t.id}
-              bg="white"
-              p="15px"
-              borderRadius="lg"
-              shadow="md"
-              // borderWidth="3px"
-              // borderColor="purple.100"
-              onClick={() => handleClickCard(t)}
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <Stack spacing="15px">
+            <Button
+              colorScheme="pink"
+              w="full"
+              onClick={() => router.push('/bankcard/add')}
+              disabled={bankcardList.length >= 5}
             >
-              <HStack justify="space-between">
-                <Text color="gray.900" fontSize="md" fontWeight="600">
-                  {t.name}
+              新增银行卡 (上限5张)
+            </Button>
+            {bankcardList?.map((t) => (
+              <Stack
+                key={t.id}
+                bg="white"
+                p="15px"
+                borderRadius="lg"
+                shadow="md"
+                // borderWidth="3px"
+                // borderColor="purple.100"
+                onClick={() => handleClickCard(t)}
+              >
+                <HStack justify="space-between">
+                  <Text color="gray.900" fontSize="md" fontWeight="600">
+                    {t.name}
+                  </Text>
+                  <Tag
+                    variant="solid"
+                    borderRadius="4px"
+                    colorScheme={
+                      t.status === BankCardStatus.On ? 'pink' : 'gray'
+                    }
+                  >
+                    {toOptionName(bankcardStatusOpts, t.status)}
+                  </Tag>
+                </HStack>
+                <Text fontSize="30px" fontWeight="600" color="purple.600">
+                  {t.account}
                 </Text>
-                <Tag
-                  variant="solid"
-                  borderRadius="4px"
-                  colorScheme={t.status === BankCardStatus.On ? 'pink' : 'gray'}
-                >
-                  {toOptionName(bankcardStatusOpts, t.status)}
-                </Tag>
-              </HStack>
-              <Text fontSize="30px" fontWeight="600" color="purple.600">
-                {t.account}
-              </Text>
-              <Text color="gray.400" fontSize="sm" fontWeight="600">
-                {t.bank} / {t.bank_name}
-              </Text>
-            </Stack>
-          ))}
-        </Stack>
+                <Text color="gray.400" fontSize="sm" fontWeight="600">
+                  {t.bank} / {t.bank_name}
+                </Text>
+              </Stack>
+            ))}
+          </Stack>
+        )}
       </Box>
       <FooterNav />
 
