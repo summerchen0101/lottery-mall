80a2b378dd93a3c310d5997539e960c6262d72b2
Aiden <aiden750107@gmail.com>
2021-06-09 20:40:33 +0800

    app-download.tsx update

--- pages/app-download.tsx
+++ pages/app-download.tsx
@@ -3,17 +3,21 @@ import Layout from '@/components/Layout'
 import TextCopy from '@/components/TextCopy'
 import { Button, ButtonProps } from '@chakra-ui/button'
 import Icon from '@chakra-ui/icon'
-import { Box, Center, Heading, Text, VStack } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/image'
+import { Box, Center, Heading, HStack, Text, VStack } from '@chakra-ui/layout'
 import { useRouter } from 'next/dist/client/router'
 import React, { useMemo } from 'react'
 import { BiX } from 'react-icons/bi'
 
 const buttonProps: ButtonProps = {
+  pos: 'relative',
   size: 'lg',
-  w: '80%',
+  w: '78%',
   h: '70px',
+  px: '4',
   colorScheme: 'brand',
-  flexDir: 'column',
+  alignItems: 'center',
+  // flexDir: 'column',
   bg: 'transparent',
   color: 'white',
   borderColor: 'white',
@@ -30,51 +34,118 @@ export default function appDownload() {
   }, [])
   return (
     <Layout>
-      <Icon
-        as={BiX}
-        fontSize="40px"
-        color="gray.500"
-        pos="fixed"
-        top="4"
-        right="4"
-        onClick={() => router.back()}
-      />
-      <Center mt="80px" mb="30px">
-        <Heading color="white">GEM100</Heading>
-      </Center>
-      <VStack spacing="5">
-        <Button
-          {...buttonProps}
-          onClick={() =>
-            window.open(
-              'https://www.akoya-jp.com/down?pckId=3cb1a355ea734eb880641d2514e04020',
-              'IOS APP',
-            )
-          }
-        >
-          <Text>下载苹果版 | for iPhone</Text>
-          <Text fontSize="sm" mt="1">
-            支援 iOS 10.0 以上 | iOS 10.0 +
+      <Box
+        w="100%"
+        h="100%"
+        bgImage="url('/img/bg-appdownload.jpg')"
+        bgPos="center"
+        bgRepeat="no-repeat"
+        bgSize="cover"
+      >
+        <Icon
+          as={BiX}
+          fontSize="40px"
+          color="#fff"
+          pos="fixed"
+          top="15px"
+          right="15px"
+          onClick={() => router.back()}
+        />
+        <Center w="full" mt="2rem">
+          <Image src="/img/logo.png" boxSize="138px" />
+        </Center>
+
+        <Center mb="10px">
+          <Text color="white" fontSize="2.1rem" fontWeight="600">
+            GEM100
           </Text>
-        </Button>
-        <Button
-          {...buttonProps}
-          onClick={() => router.push('/app/android.apk')}
+        </Center>
+        <VStack spacing="5">
+          <HStack
+            {...buttonProps}
+            onClick={() =>
+              window.open(
+                'https://www.akoya-jp.com/down?pckId=3cb1a355ea734eb880641d2514e04020',
+                'IOS APP',
+              )
+            }
+          >
+            <Box
+              as="i"
+              className="ic-iOS iconfont"
+              fontSize="28px"
+              w="33px"
+              lineHeight="33px"
+              textAlign="center"
+            ></Box>
+            <VStack spacing="0" alignItems="flex-start">
+              <Text fontSize="1.1rem" fontWeight="600">
+                下载苹果版 | for iPhone
+              </Text>
+              <Text fontSize="sm" mt="1">
+                支援 iOS 10.0 以上 | iOS 10.0 +
+              </Text>
+            </VStack>
+          </HStack>
+          <HStack
+            {...buttonProps}
+            onClick={() => router.push('/app/android.apk')}
+          >
+            <Box
+              as="i"
+              className="ic-android iconfont"
+              fontSize="28px"
+              w="33px"
+              lineHeight="33px"
+              textAlign="center"
+            ></Box>
+            <VStack spacing="0" alignItems="flex-start">
+              <Text fontSize="1.1rem" fontWeight="600">
+                下载安卓版 | for Android
+              </Text>
+              <Text fontSize="sm" mt="1">
+                支援安卓 5.0 以上 | Android 5.0 +
+              </Text>
+            </VStack>
+          </HStack>
+          <TextCopy text={website}>
+            <HStack {...buttonProps}>
+              <Box
+                as="i"
+                className="ic-wechat iconfont"
+                fontSize="28px"
+                w="33px"
+                lineHeight="33px"
+                textAlign="center"
+              ></Box>
+              <VStack spacing="0" alignItems="flex-start">
+                <Text fontSize="1.1rem" fontWeight="600">
+                  微信玩家请用手机浏览器
+                </Text>
+                <Text fontSize="sm" mt="1">
+                  点击复制网址 | click to copy URL
+                </Text>
+              </VStack>
+            </HStack>
+          </TextCopy>
+        </VStack>
+        <VStack
+          pos="fixed"
+          bottom="0"
+          padding="15px 8px"
+          bg="rgba(0,0,0,.5)"
+          color="#8a8a8a"
+          fontSize="xs"
+          spacing="0"
+          alignItems="flex-start"
         >
-          <Text>下载安卓版 | for Android</Text>
-          <Text fontSize="sm" mt="1">
-            支援安卓 5.0 以上 | Android 5.0 +
+          <Text>若APP使用上有困难，建议使用 Chrome 浏览器，或联系客服人员</Text>
+          <Text>
+            We recommend Chrome, or contact customer service if you have any
+            problem.
           </Text>
-        </Button>
-        <TextCopy text={website}>
-          <Button {...buttonProps}>
-            <Text>微信玩家请用手机浏览器</Text>
-            <Text fontSize="sm" mt="1">
-              点击复制网址 | click to copy URL
-            </Text>
-          </Button>
-        </TextCopy>
-      </VStack>
+        </VStack>
+      </Box>
     </Layout>
   )
 }
