13a62b6971db71ee189f39874fd8e91c2ca8ec2c
Summer Chen <mp4bj6ruby@gmail.com>
2021-06-09 12:56:41 +0800

    app-download page

---
+++ pages/app-download.tsx
@@ -0,0 +1,69 @@
+import HeaderTitleBar from '@/components/HeaderTitleBar'
+import Layout from '@/components/Layout'
+import TextCopy from '@/components/TextCopy'
+import { Button, ButtonProps } from '@chakra-ui/button'
+import { Box, Center, Heading, Text, VStack } from '@chakra-ui/layout'
+import { useRouter } from 'next/dist/client/router'
+import React, { useMemo } from 'react'
+
+const buttonProps: ButtonProps = {
+  size: 'lg',
+  w: '80%',
+  h: '70px',
+  colorScheme: 'brand',
+  flexDir: 'column',
+  bg: 'transparent',
+  color: 'white',
+  borderColor: 'white',
+  borderRadius: 'full',
+  borderWidth: '1px',
+}
+
+export default function appDownload() {
+  const router = useRouter()
+  const website = useMemo(() => {
+    if (process.browser) {
+      return `${location.protocol}//${location.host}`
+    }
+  }, [])
+  return (
+    <Layout>
+      <Center mt="80px" mb="30px">
+        <Heading color="white">GEM100</Heading>
+      </Center>
+      <VStack spacing="5">
+        <Button
+          {...buttonProps}
+          onClick={() =>
+            window.open(
+              'https://www.akoya-jp.com/down?pckId=3cb1a355ea734eb880641d2514e04020',
+              'IOS APP',
+            )
+          }
+        >
+          <Text>下载苹果版 | for iPhone</Text>
+          <Text fontSize="sm" mt="1">
+            支援 iOS 10.0 以上 | iOS 10.0 +
+          </Text>
+        </Button>
+        <Button
+          {...buttonProps}
+          onClick={() => router.push('/app/android.apk')}
+        >
+          <Text>下载安卓版 | for Android</Text>
+          <Text fontSize="sm" mt="1">
+            支援安卓 5.0 以上 | Android 5.0 +
+          </Text>
+        </Button>
+        <TextCopy text={website}>
+          <Button {...buttonProps}>
+            <Text>微信玩家请用手机浏览器</Text>
+            <Text fontSize="sm" mt="1">
+              点击复制网址 | click to copy URL
+            </Text>
+          </Button>
+        </TextCopy>
+      </VStack>
+    </Layout>
+  )
+}
